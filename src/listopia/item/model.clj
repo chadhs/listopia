fcb2ed11f2961662d37c7ae84a079d745ed970d6
Chad Stovern <chad@stovern.me>
2017-09-16 07:54:24 -0500

    implemented hugsql for query handling:
    - replaced clojure.jdbc with hugsql dep
    - created plain sql queries per hugsql spec
    - replaced jdbc query functions with hugsql call to pull in sql
    - updated handler calls to pass maps per hugsql spec

--- src/listopia/item/model.clj
+++ src/listopia/item/model.clj
@@ -1,34 +1,4 @@
 (ns listopia.item.model
-  (:require [clojure.java.jdbc :as db]))
+  (:require [hugsql.core :as hugsql]))
 
-(defn create-item! [db name description]
-  (:id (first (db/query
-               db
-               ["insert into items (name, description)
-                 values (?, ?)
-                 returning id"
-                name
-                description]))))
-
-(defn update-item! [db id checked]
-  (= [1] (db/execute!
-          db
-          ["update items
-            set checked = ?
-            where id = ?"
-           checked
-           id])))
-
-(defn delete-item! [db id]
-  (= [1] (db/execute!
-          db
-          ["delete from items
-            where id = ?"
-           id])))
-
-(defn read-items [db]
-  (db/query
-   db
-   ["select id, name, description, checked, date_created
-     from items
-     order by date_created"]))
+(hugsql/def-db-fns "listopia/item/sql/item.sql")
