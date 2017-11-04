f420c92bbdb33668c90ddd30d7325939fffcdff8
Chad Stovern <chad@stovern.me>
2017-11-04 18:00:39 -0500

    WIP: functional testing
    - mocking out tests and then working on implementing them leveraging
      migratus to avoid state issues

--- test/listopia/core_test.clj
+++ test/listopia/core_test.clj
@@ -1,7 +1,47 @@
 (ns listopia.core-test
-  (:require [clojure.test :refer :all]
-            [listopia.core :refer :all]))
+  (:require [clojure.test  :refer :all]
+            [listopia.core :refer :all]
+            [listopia.db   :refer [database-url]]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 0))))
+
+;; TODO
+;; first get non db test running
+;; then wire in migratus etc
+
+
+(deftest verify-test-db
+  (testing "verify we're using the test db"
+    (is
+     ;; (= database-url "jdbc:postgresql://localhost/listopia-test")
+     (= database-url database-url)
+     )))
+
+
+(deftest  create-list
+  (testing "create-list"
+    (is (= :test :test))))
+
+
+(deftest  delete-list
+  (testing "delete-list"
+    (is (= :test :test))))
+
+
+(deftest  create-item
+  (testing "create-item"
+    (is (= :test :test))))
+
+
+(deftest  check-item
+  (testing "check-item"
+    (is (= :test :test))))
+
+
+(deftest uncheck-item
+  (testing "uncheck-item"
+    (is (= :test :test))))
+
+
+(deftest  delete-item
+  (testing "delete-item"
+    (is (= :test :test))))
