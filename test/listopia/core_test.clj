409daf7bd512632a6674c40a3ed8eb015fdf7df7
Chad Stovern <chad@stovern.me>
2017-11-28 09:19:32 -0600

    initial workup of mocking requests and scaffolding out test
    - wrote a mocking function for dealing with requests generation for
      passing to handlers. initially pulled in ring-mock, but for now my own
      cs-http-request-mock is more suited to what i need
    - lein test and cider test aren't both using the test profile so i'm
      manually defining the test db for now in the core test file
    - my first list creation test is working well with my mock funtion
    - i've defined mock tests for the other crud operations with comments,
      will tackle those next

--- test/listopia/core_test.clj
+++ test/listopia/core_test.clj
@@ -1,47 +1,78 @@
 (ns listopia.core-test
-  (:require [clojure.test  :refer :all]
-            [listopia.core :refer :all]
-            [listopia.db   :refer [database-url]]))
+  (:require [clojure.test                 :refer :all])
+  (:require [listopia.core                :refer :all]
+            [listopia.list.model          :as    list.model]
+            [listopia.list.handler        :as    list.handler]))
+
+
+;; workaround
+(def database-url "jdbc:postgresql://localhost/listopia-test")
+(def db database-url)
 
 
 ;; TODO
-;; first get non db test running
-;; then wire in migratus etc
+;;; then wire in migratus setup ↓
+;;; dropdb listopia-test && createdb listopia-test && lein with-profile test migratus migrate
+
+
+;; helper functions
+(defn cs-http-request-mock
+  "creates a request defaulting to http"
+  [& {:keys [scheme server-port uri request-method params]
+      :or {scheme :http server-port 80 request-method :get}}]
+  (let [base-mock {:protocol "HTTP/1.1"
+                   :scheme scheme
+                   :server-port server-port
+                   :server-name "localhost"
+                   :remote-addr "localhost"
+                   :headers {"host" "localhost"}
+                   :uri uri
+                   :request-method request-method}]
+    (if params
+      (assoc base-mock :params params)
+      base-mock)))
 
 
 (deftest verify-test-db
   (testing "verify we're using the test db"
     (is
-     ;; (= database-url "jdbc:postgresql://localhost/listopia-test")
-     (= database-url database-url)
-     )))
+     (= database-url "jdbc:postgresql://localhost/listopia-test"))))
 
 
-(deftest  create-list
-  (testing "create-list"
-    (is (= :test :test))))
+(deftest test-create-list
+  (is (let [request (cs-http-request-mock 
+                     :uri "/list/create" 
+                     :request-method :post 
+                     :params {:name "foo" :description "bar"})]
+        (= (get (list.handler/handle-create-list! request) :status)
+           302))))
 
 
-(deftest  delete-list
+;; create a list, fetch id, delete the list
+(deftest delete-list
   (testing "delete-list"
     (is (= :test :test))))
 
 
-(deftest  create-item
+;; create a list, fetch id, create item with list id
+(deftest create-item
   (testing "create-item"
     (is (= :test :test))))
 
 
-(deftest  check-item
+;; create a list, fetch id, create item with list id, fetch item id, set checked true
+(deftest check-item
   (testing "check-item"
     (is (= :test :test))))
 
 
+;; create a list, fetch id, create item with list id, fetch item id, set checked false
 (deftest uncheck-item
   (testing "uncheck-item"
     (is (= :test :test))))
 
 
-(deftest  delete-item
+;; create a list, fetch id, create item with list id, fetch item id, delete item
+(deftest delete-item
   (testing "delete-item"
     (is (= :test :test))))
